6fece4db9c3edfbe1d7a165176ab0a76637fbcc5
Boyd Trolinger <boyd.trolinger@gmail.com>
2021-11-20 10:05:46 -0800

    First commit

---
+++ docs/fall-2021/q_and_a.md
@@ -0,0 +1,13 @@
+---
+sidebar_position: 3
+pagination_next: null
+---
+
+# Questions and Answers
+
+<details>
+  <summary>
+  	<strong>What is the answer to the thing?</strong>
+  </summary>
+  <p>This is part of the answer to the thing.</p>
+</details>
---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BC Software Industry Panel',
+  tagline: 'Hello world something something something',
+  url: 'https://github.com/jboydt/bc',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'jboydt', // Usually your GitHub org/user name.
+  projectName: 'bc', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/jboydt/bc/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/jboydt/bc/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Software Industry Panel',
+        logo: {
+          alt: 'software industry panel logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'events',
+            position: 'left',
+            label: 'Events',
+          },
+          {to: '/about', label: 'About', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          // {
+          //   title: 'More',
+          //   items: [
+          //     {
+          //       label: 'Blog',
+          //       to: '/blog',
+          //     },
+          //     {
+          //       label: 'GitHub',
+          //       href: 'https://github.com/facebook/docusaurus',
+          //     },
+          //   ],
+          // },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Software Industry Panel. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
